89d03a239a1207b995a3efeff0bf8f61c87d5540
thegoatmufasa <tserebour1@gmail.com>
2024-05-22 15:12:37 +0000

    fixed some of the functions in services

--- src/services.rs
+++ src/services.rs
@@ -38,7 +38,7 @@ format!("login route")
 async fn create_user(state:Data<AppState>, body: Json<User>) -> impl Responder {
 
     match sqlx::query_as::<_,User>(
-        "INSERT INTO bra_fie_users (username, email, password) VALUES ($1, $2, $3) RETURNING id,username, email, password",
+        "INSERT INTO users (username, email, password) VALUES ($1, $2, $3) RETURNING id,username, email, password",
     )
     .bind(body.fullname.to_string())
     .bind(body.email.to_string())
@@ -83,7 +83,7 @@ struct LoginCredentials {
 #[derive(Debug)]
 #[derive(Serialize,Deserialize, Clone,FromRow)]
 struct BraFieLoginCredentials {
-username: String,
+email: String,
 password: String,
 }
 
@@ -137,38 +137,38 @@ return Err(actix_err);
 
 
 #[post("/bra_fie_login")]
-async fn bra_fie_sign_in(state: Data<AppState>, credentials: Json<LoginCredentials>) -> Result<HttpResponse, actix_web::Error> {
-let username = credentials.username.clone();
-let password = credentials.password.clone();
+async fn bra_fie_sign_in(state: Data<AppState>, credentials: Json<BraFieLoginCredentials>) -> Result<HttpResponse, actix_web::Error> {
+    let username = credentials.email.clone();
+    let password = credentials.password.clone();
 
-// 1. Validate email and password (optional)
-// You can add logic here to validate email format or password length
+    // 1. Validate email and password (optional)
+    // You can add logic here to validate email format or password length
 
-let user_result = sqlx::query_as::<_, User>(
-"SELECT * FROM bra_fie_users WHERE username = $1;",
-)
-.bind(username)
-.fetch_one(&state.db)
-.await; // Use await? to propagate potential errors
+    let user_result = sqlx::query_as::<_, BraFieUser>(
+        "SELECT * FROM bra_fie_users WHERE email = $1;",
+    )
+    .bind(username)
+    .fetch_one(&state.db)
+    .await; // Use await? to propagate potential errors
 
 
-match user_result {
-Ok(user) => {
-if user.password != password {
-    Ok(HttpResponse::Ok().json("Invalid Username and Password"))
-    
-}else {
+    match user_result {
+    Ok(user) => {
+        if user.password != password {
+            Ok(HttpResponse::Ok().json("Invalid Username and Password"))
+            
+        }else {
 
-    Ok(HttpResponse::Ok().json(&user))
+            Ok(HttpResponse::Ok().json(&user))
 
-}
-},
-Err(err) => {
-// Convert sqlx::Error to actix_web::Error
-let actix_err = actix_web::error::ErrorInternalServerError(err.to_string());
-return Err(actix_err);
-}
-}
+    }
+    },
+    Err(err) => {
+        // Convert sqlx::Error to actix_web::Error
+        let actix_err = actix_web::error::ErrorInternalServerError(err.to_string());
+        return Err(actix_err);
+    }
+    }
 
 
 
@@ -181,8 +181,8 @@ return Err(actix_err);
 #[post("/bra_fie_create")]
 async fn bra_fie_create_user(state:Data<AppState>, body: Json<BraFieUser>) -> impl Responder {
 
-    match sqlx::query_as::<_,User>(
-        "INSERT INTO bra_fie_users (fullname, email, phone, password) VALUES ($1, $2,$3 $4) RETURNING id,fullname, email,phone, password",
+    match sqlx::query_as::<_,BraFieUser>(
+        "INSERT INTO bra_fie_users (fullname, email, phone, password) VALUES ($1, $2,$3, $4) RETURNING id,fullname, email,phone, password",
     )
     .bind(body.fullname.to_string())
     .bind(body.email.to_string())
